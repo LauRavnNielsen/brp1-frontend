ea1da7c40b1f9709db5b7b450e39ecc2fbb0da3a
Tamas12345678 <43003631+Tamas12345678@users.noreply.github.com>
2022-06-01 10:06:20 +0200

    search for non-existing recipe

--- cypress/integration/createmeals.spec.js
+++ cypress/integration/createmeals.spec.js
@@ -36,4 +36,19 @@ describe('Tracking dashboard', () => {
         cy.get('[title="Breakfast"]').click()
         cy.get('[title="Dinner"]').click({ force: true }).should('exist')
     })
+    it.only('searches for non-existing recipes', () => {
+        cy.login('test','test')
+        cy.url().should('eq','http://localhost:3000/');
+        cy.contains('Tracking').click()
+        cy.get('input').first().type('pizza').click()
+        cy.contains('Search').click()
+        cy.contains('0').should('exist')
+        cy.contains('1').should('exist')
+        cy.contains('2').should('exist')
+        cy.get('input').first().type('pizza1234').click()
+        cy.contains('Search').click()
+        cy.contains('0').should('not.exist')
+        cy.contains('1').should('not.exist')
+        cy.contains('2').should('not.exist')
+    })
 });
